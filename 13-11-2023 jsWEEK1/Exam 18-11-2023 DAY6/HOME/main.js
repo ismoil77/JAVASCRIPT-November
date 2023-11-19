12f7c5583d5689ac73b277d8e8deeeb31f0a285f
ismoil77 <braboev@gmail.com>
2023-11-19 22:07:54 +0500

    sdfv

---
+++ 13-11-2023 jsWEEK1/Exam 18-11-2023 DAY6/HOME/main.js
@@ -0,0 +1,24 @@
+console.log(null == undefined);
+console.log(null == 0);
+console.log(null == false);
+console.log(null == NaN);
+
+console.log(undefined == null);
+console.log(undefined == 0);
+console.log(undefined == false);
+console.log(undefined == NaN);
+
+console.log(0 == undefined);
+console.log(0 == null);
+console.log(0 == false);
+console.log(0 == NaN);
+
+console.log(false == undefined);
+console.log(false == 0);
+console.log(false == null);
+console.log(false == NaN);
+
+console.log(NaN == undefined);
+console.log(NaN == 0);
+console.log(NaN == false);
+console.log(NaN == null);
